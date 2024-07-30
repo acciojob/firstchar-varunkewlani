1670aeef86348a89a3a83ca9e188b7ab1295a688
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-30 15:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstChar(text) {
 	let trimmed=text.trim()
-	return trimmed[0]
-
+	return trimmed.charAt(0)
+}
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
