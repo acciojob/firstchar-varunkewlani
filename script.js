bb16861130afe4ecbc5bcb658459b13088e18147
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-30 13:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstChar(text) {
+	let c=0
+	for(let i=0;i<text.length;i++)
+		{
+			if(text.charAt(i)===' ')
+				c++
+		}
+	if(c==text.length)
+		return "''"
+	else if(text.length==0)
+		return "''"
+    else
+	{
+		return (text.trim()).charAt(0)
+	}
   // your code here
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
